35aedf65b84588b51fcd5e3632b31f6e44ecbb1e
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-23 10:53:34 -0700

    app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 
+import CharacterList from './components/CharacterList';
+import LocationsList from './components/LocationsList';
+import EpisodeList from './components/EpisodeList';
+import { Route } from 'react-router-dom';
+
+
+
 
 export default function App() {
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
+      <TabNav />
+      
+      <Route exact path="/CharacterList/.id" component={CharacterList} /> 
+      <Route exact path="/LocationsList/.id" component={LocationsList} />
+      <Route exact path="/EpisodeList/.id" component={EpisodeList} />
     </main>
   );
 }
