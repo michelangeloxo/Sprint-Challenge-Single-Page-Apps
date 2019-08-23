ded479832bc5d2b19672422b1832c19ac1fd27e3
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-23 10:51:38 -0700

    searchform

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 
+
 export default function SearchForm({ onSearch }) {
-  // STRETCH TODO: Add stateful logic for query/form data
+  // TODO: Add stateful logic for query/form data
+  const [search, setSearch] = useState('');
+  
+
+  const handleInputChange = event => {
+    setSearch(event.target.value)
+  };
+
   return (
     <section className="search-form">
-      <form onSubmit={() => onSearch(name)}>
+      <form onSubmit={(event) => onSearch(search, event)}>
         <input
           onChange={handleInputChange}
           placeholder="name"
-          value={name}
+          value={search}
           name="name"
         />
         <button type="submit">Search</button>
