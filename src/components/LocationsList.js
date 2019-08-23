161b5629fba410de66d435dbbc484225a0d568ba
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-23 10:50:22 -0700

    location list

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import LocationCard from './LocationCard';
 
-export default function LocationsList() {}
+export default function LocationsList() {
+
+    const [location, setLocation] = useState()
+    useEffect(() => {   
+       
+        axios
+          .get('https://rickandmortyapi.com/api/location/')
+          .then(response => {
+            setLocation(response.data.results);
+          })
+          .catch(error => {
+            console.error('Server Error', error);
+          });
+      
+
+    }, []);
+  if(!location){ return null; }
+    return (
+      <section className="location-list grid-view">
+        {location.map(location1 => (
+          <LocationCard key={location1.id} location={location1} />
+        ))}
+      </section>
+    );
+  }
